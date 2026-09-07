90413c0c586843ac4253462e144ea38ecea01aec
ayechannit <60909089+ayechannit@users.noreply.github.com>
2026-09-07 21:49:35 +0700

    remove admin

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -3,8 +3,8 @@ import '../styles/Login.css';
 import { API_ROOT } from '../config/api';
 
 const Login = ({ onLoginSuccess }) => {
-  const [username, setUsername] = useState('admin');
-  const [password, setPassword] = useState('admin');
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
   const [loading, setLoading] = useState(false);
   const [error, setError] = useState('');
   const [loggedInUser, setLoggedInUser] = useState(null);
